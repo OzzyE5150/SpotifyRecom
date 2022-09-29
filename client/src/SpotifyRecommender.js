cd38e655fb2ea590391d153a433d0b8f140b78f5
gpan <32671209+OzzyE5150@users.noreply.github.com>
2022-09-29 09:57:32 +0300

    implement the search bar in SpotifyRecommender.js

--- client/src/SpotifyRecommender.js
+++ client/src/SpotifyRecommender.js
@@ -1,5 +1,37 @@
-import React from 'react'
-const SpotifyRecommender = () => {
- return ( <div>Hello I'm logged in</div> )
-}
+import React, { useState } from 'react';
+import './App.css';
+import { Grid, TextField, Button } from '@material-ui/core';
+import { Search } from '@material-ui/icons';
+
+const SpotifyRecommender = ({ auth }) => {
+    const { token } = auth;
+    const [searchResults, setSearchResults] = useState([]);
+    const [searchString, setSearchString] = useState('');
+    return (
+        <div className={"App"}>
+            <Grid container style={{ padding: 20 }} spacing={1}>
+                <Grid item xs={12}>
+                    Spotify Recommender
+                </Grid>
+                <Grid item xs={6}>
+                    <Grid item xs={12} style={{ display: 'flex', flexDirection: 'row' }}>
+                        <TextField
+                            variant={'outlined'}
+                            label={"Search"}
+                            style={{ backgroundColor: 'white' }}
+                            fullWidth
+                            onChange={event => setSearchString(event.target.value)}
+                            value={searchString}
+                        />
+                        <Button style={{ backgroundColor: '#ff905b' }}>
+                            <Search />
+                        </Button>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                </Grid>
+            </Grid>
+        </div>
+    );
+};
 export default SpotifyRecommender;

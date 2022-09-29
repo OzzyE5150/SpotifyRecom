3e3fd78cfa9d11eb354230edccd23add221e5ae8
gpan <32671209+OzzyE5150@users.noreply.github.com>
2022-09-29 10:47:21 +0300

    Update SliderBoard.js

--- client/src/components/SliderBoard.js
+++ client/src/components/SliderBoard.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {Grid, Typography, Slider, Checkbox} from '@material-ui/core';
 
 // initial states
-const knobs = {
+const sliders = {
   'acousticness': {
     value: [0, 1],
     enabled: false,
@@ -73,3 +73,69 @@ const knobs = {
     step: 0.01
   },
 };
+
+const SliderBoard = ({onChange}) => {
+    const [boardValues, setBoardValues] = useState(sliders);
+  
+    const handleChange = (slider, value) => {
+      const newBoardValues = {...boardValues};
+      newBoardValues[slider].value = value;
+      setBoardValues(newBoardValues);
+    };
+  
+    const toggleSlider = (slider, value) => {
+      const newBoardValues = {...boardValues};
+      newBoardValues[slider].enabled = value;
+      setBoardValues(newBoardValues);
+    };
+  
+    useEffect(() => {
+      onChange(boardValues);
+    }, [onChange, boardValues]);
+  
+    return (
+     <Grid container spacing={2} style={{padding: 10}}>
+       {Object.keys(sliders).map(slider => (
+         <Grid item xs={12}>
+           <div style={{display: 'flex', flexDirection: 'row'}}>
+             <Checkbox
+               checked={boardValues[slider].enabled}
+               onChange={(event, newValue) => toggleSlider(slider, newValue)}
+             />
+             <div style={{flex: 1}}>
+               <Typography>
+                 {slider}
+               </Typography>
+               <Grid container spacing={1}>
+                 <Grid item>
+                   <Typography>
+                     Min
+                   </Typography>
+                 </Grid>
+                 <Grid item xs>
+                   <Slider
+                     disabled={!boardValues[slider].enabled}
+                     value={boardValues[slider].value}
+                     onChange={(event, newValue) => handleChange(slider, newValue)}
+                     valueLabelDisplay={"on"}
+                     aria-labelledby={"range-slider"}
+                     min={sliders[slider].min}
+                     max={sliders[slider].max}
+                     step={sliders[slider].step}
+                   />
+                 </Grid>
+                 <Grid item>
+                   <Typography>
+                     Max
+                   </Typography>
+                 </Grid>
+               </Grid>
+             </div>
+           </div>
+         </Grid>
+       ))}
+     </Grid>
+    );
+  };
+  
+  export default SliderBoard
